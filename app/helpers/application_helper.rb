52f315c080ffb99239feec28017b78daa9e6dc64
Tiago Amaro <tiagocis@gmail.com>
2013-05-08 07:35:09 -0300

    creating nav_link helper (without spec)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	# TODO write spec
+	def nav_link(link_text, link_path, highlights_on = false)
+		class_name = current_page?(link_path) ? 'active' : nil
+		if highlights_on
+			class_name = request.fullpath.include?(highlights_on) ? 'active' : nil
+		end
+
+		content_tag(:li, :class => class_name) do
+			link_to link_text, link_path
+		end
+	end
 end
